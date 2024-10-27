763e2d078bc489367a30de141c998e2bc5e159d3
ADMIN\Administrator <tien.nguyen20020713@gmail.com>
2024-10-27 19:08:12 +0700

    Upload all initial files and the implemented source code of XArrayList, DLinkedList, dataset, dataloader

--- Code/include/loader/dataloader.h
+++ Code/include/loader/dataloader.h
@@ -1,79 +1,215 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
- */
-
-/* 
- * File:   dataloader.h
- * Author: ltsach
- *
- * Created on September 2, 2024, 4:01 PM
- */
-
 #ifndef DATALOADER_H
 #define DATALOADER_H
-#include "tensor/xtensor_lib.h"
 #include "loader/dataset.h"
+#include "tensor/xtensor_lib.h"
 
 using namespace std;
 
-template<typename DType, typename LType>
-class DataLoader{
+template <typename DType, typename LType>
+class DataLoader
+{
 public:
-    class Iterator; //forward declaration for class Iterator
-    
+    class Iterator;
+
 private:
-    Dataset<DType, LType>* ptr_dataset;
-    int batch_size;
+    Dataset<DType, LType>* ptr_dataset; // pointer to dataset
+    int batch_size; // size of each batch
     bool shuffle;
     bool drop_last;
-    int nbatch;
-    ulong_tensor item_indices;
-    int m_seed;
-    
-public:
-    DataLoader(Dataset<DType, LType>* ptr_dataset, 
-            int batch_size, bool shuffle=true, 
-            bool drop_last=false, int seed=-1)
-                : ptr_dataset(ptr_dataset), 
-                batch_size(batch_size), 
-                shuffle(shuffle),
-                m_seed(seed){
-            nbatch = ptr_dataset->len()/batch_size;
-            item_indices = xt::arange(0, ptr_dataset->len());
-    }
-    virtual ~DataLoader(){}
-    
-    //New method: from V2: begin
-    int get_batch_size(){ return batch_size; }
-    int get_sample_count(){ return ptr_dataset->len(); }
-    int get_total_batch(){return int(ptr_dataset->len()/batch_size); }
-    
-    //New method: from V2: end
-    /////////////////////////////////////////////////////////////////////////
-    // The section for supporting the iteration and for-each to DataLoader //
-    /// START: Section                                                     //
-    /////////////////////////////////////////////////////////////////////////
-public:
-    Iterator begin(){
-        //YOUR CODE IS HERE
-    }
-    Iterator end(){
-        //YOUR CODE IS HERE
-    }
-    
-    //BEGIN of Iterator
+    xt::xarray<int> indices; // array contains the data index
+    int total_batch;
+    int seed; // used to determine the dataset will be shuffled or not
+    // TODO : add more member variables to support the iteration
 
-    //YOUR CODE IS HERE: to define iterator
+public:
+    DataLoader(Dataset<DType, LType>* ptr_dataset, int batch_size, bool shuffle = true, bool drop_last = false, int seed = -1)
+    {
+        // TODO implement
+        this -> ptr_dataset = ptr_dataset;
+        this -> batch_size = batch_size;
+        this -> shuffle = shuffle;
+        this -> drop_last = drop_last;
+        this -> seed = seed;
 
-    //END of Iterator
-    
-    /////////////////////////////////////////////////////////////////////////
-    // The section for supporting the iteration and for-each to DataLoader //
-    /// END: Section                                                       //
-    /////////////////////////////////////////////////////////////////////////
+        int num_sample = this -> ptr_dataset -> len(); // number of element in the dataset
+        this -> indices = xt::arange(0, num_sample);
+        this -> total_batch = num_sample/(this -> batch_size);
+
+        if(this -> shuffle == true)
+        {   
+            if(this -> seed >= 0)
+            {
+                xt::random::seed(this -> seed);
+                xt::random::shuffle(this -> indices);
+            }
+            else
+            {
+                xt::random::shuffle(this -> indices);
+            }
+        }
+
+        if(this -> drop_last == true)
+        {
+            if(this -> batch_size <= num_sample)
+            {
+                int remove_element;
+                remove_element = num_sample - total_batch * batch_size;
+                this -> indices = xt::view(this -> indices, xt::range(0, num_sample - remove_element));
+            }
+            else
+            {
+                this -> total_batch = 0;
+                this -> indices = xt::view(this -> indices, xt::range(0, 0));
+            }
+        }
+        else
+        {
+            if(this -> batch_size <= num_sample)
+            {
+                this -> indices = xt::view(this -> indices, xt::range(0, num_sample));
+            }
+            else
+            {
+                this -> total_batch = 0;
+                this -> indices = xt::view(this -> indices, xt::range(0, 0));
+            }
+        }
+    }
+
+    virtual ~DataLoader()
+    {
+        // TODO implement
+    }
+
+    Iterator begin()
+    {
+        return Iterator(this -> ptr_dataset, this -> indices, 0, this -> batch_size, this -> total_batch);
+    }
+
+    Iterator end()
+    {
+        return Iterator(this -> ptr_dataset, this -> indices, this -> total_batch, this -> batch_size, this -> total_batch);
+    }
+
+    // TODO implement foreach
+    class Iterator
+    {
+    private:
+        Dataset<DType, LType>* ptr_dataset;
+        xt::xarray<int> indices;
+        int batch_index;
+        int batch_size;
+        int total_batch;
+
+    public:
+    // TODO implement constructor
+        Iterator(Dataset<DType, LType>* ptr_dataset, xt::xarray<int> indices, int batch_index, int batch_size, int total_batch)
+        {
+            this -> ptr_dataset = ptr_dataset;
+            this -> batch_size = batch_size;
+            this -> indices = indices;
+            this -> total_batch = total_batch;
+            this -> batch_index = batch_index;
+        }
+
+        Iterator& operator=(const Iterator& iterator)
+        {
+            // TODO implement
+            if(this != &iterator)
+            {
+                this -> ptr_dataset = iterator.ptr_dataset;
+                this -> batch_size = iterator.batch_size;
+                this -> indices = iterator.indices;
+                this -> total_batch = iterator.total_batch;
+                this -> batch_index = iterator.batch_index;
+            }
+
+            return *this;
+        }
+
+        Iterator& operator++()
+        {
+            // TODO implement
+            this -> batch_index++;
+            return *this;
+        }
+
+        Iterator operator++(int)
+        {
+            // TODO implement
+            Iterator foo = *this;
+            this -> batch_index++;
+            return foo;
+        }
+
+        bool operator!=(const Iterator& other) const
+        {
+            // TODO implement
+            return this -> batch_index != other.batch_index;
+        }
+
+        Batch<DType, LType> operator*() const
+        {
+            // TODO implement
+            if(indices.size() > 0)
+            {
+                int begin_index = this -> batch_index * this -> batch_size;
+                int end_index = begin_index + this -> batch_size;
+
+                xt::svector<unsigned long> fixed_size_data;
+                xt::svector<unsigned long> fixed_size_label;
+                
+                xt::xarray<DType> batch_data;
+                xt::xarray<LType> batch_label;
+
+                if(this -> batch_index == this -> total_batch - 1)
+                {
+                    end_index = indices.size();
+                    fixed_size_data = this -> ptr_dataset -> get_data_shape();
+                    fixed_size_label = this -> ptr_dataset -> get_label_shape();
+
+                    fixed_size_data[0] = end_index - begin_index;
+                    batch_data = xt::empty<DType>(fixed_size_data);
+
+                    fixed_size_label[0] = end_index - begin_index;
+                    batch_label = xt::empty<LType>(fixed_size_label);
+                }
+                else
+                {
+                    end_index = begin_index + this -> batch_size;
+                    fixed_size_data = this -> ptr_dataset -> get_data_shape();
+                    fixed_size_label = this -> ptr_dataset -> get_label_shape();
+
+                    fixed_size_data[0] = batch_size;
+                    batch_data = xt::empty<DType>(fixed_size_data);
+
+                    fixed_size_label[0] = batch_size;
+                    batch_label = xt::empty<LType>(fixed_size_label);
+                }
+
+                for(int i = begin_index; i < end_index; i++)
+                {
+                    int idx = this -> indices[i];
+                    DataLabel<DType, LType> dataset_index = this -> ptr_dataset -> getitem(idx);
+
+                    if(dataset_index.getData().size() > 0)
+                    {
+                        xt::view(batch_data, i - begin_index, xt::all()) = dataset_index.getData();
+                    }
+                    else batch_data = xt::empty<DType>({});
+
+                    if(fixed_size_label.size() > 0)
+                    {
+                        xt::view(batch_label, i - begin_index, xt::all()) = dataset_index.getLabel();
+                    }
+                    else batch_label = xt::empty<LType>({});
+                }
+
+                return Batch<DType, LType>(batch_data, batch_label);
+            }
+            return Batch<DType, LType>(xt::xarray<DType>(), xt::xarray<LType>());
+        }
+    };
 };
 
-
 #endif /* DATALOADER_H */
-
--- Code/include/loader/dataset.h
+++ Code/include/loader/dataset.h
@@ -1,15 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
- */
-
-/* 
- * File:   dataset.h
- * Author: ltsach
- *
- * Created on September 2, 2024, 3:59 PM
- */
-
 #ifndef DATASET_H
 #define DATASET_H
 #include "tensor/xtensor_lib.h"
@@ -59,7 +47,8 @@ public:
 
 //////////////////////////////////////////////////////////////////////
 template<typename DType, typename LType>
-class TensorDataset: public Dataset<DType, LType>{
+class TensorDataset: public Dataset<DType, LType>
+{
 private:
     xt::xarray<DType> data;
     xt::xarray<LType> label;
@@ -71,38 +60,58 @@ public:
      * 1. data, label;
      * 2. data_shape, label_shape
     */
-    TensorDataset(xt::xarray<DType> data, xt::xarray<LType> label){
+    TensorDataset(xt::xarray<DType> data, xt::xarray<LType> label)
+    {
         /* TODO: your code is here for the initialization
          */
+        this -> data = data;
+        this -> label = label;
+        this -> data_shape = data.shape();
+        this -> label_shape = label.shape();
     }
     /* len():
      *  return the size of dimension 0
     */
-    int len(){
+    int len()
+    {
         /* TODO: your code is here to return the dataset's length
          */
-        return 0; //remove it when complete
+        return data.shape()[0]; //remove it when complete
     }
     
     /* getitem:
      * return the data item (of type: DataLabel) that is specified by index
      */
-    DataLabel<DType, LType> getitem(int index){
+    DataLabel<DType, LType> getitem(int index)
+    {
         /* TODO: your code is here
          */
+        if(index < 0 || index >= len()) throw std::out_of_range("Index is out of range!");
+
+        xt::xarray<DType> single_data = xt::view(data, index, xt::all());
+        xt::xarray<LType> single_label;
+
+        if(label.shape().size() > 0)    
+        {
+            single_label = xt::view(label, index, xt::all());
+        }
+        else single_label = label;
+        
+        return DataLabel<DType, LType>(single_data, single_label);
     }
     
-    xt::svector<unsigned long> get_data_shape(){
+    xt::svector<unsigned long> get_data_shape()
+    {
         /* TODO: your code is here to return data_shape
          */
+        return data_shape;
     }
-    xt::svector<unsigned long> get_label_shape(){
+    xt::svector<unsigned long> get_label_shape()
+    {
         /* TODO: your code is here to return label_shape
          */
+        return label_shape;
     }
 };
 
-
-
 #endif /* DATASET_H */
-
