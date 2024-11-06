e87ca8275df4fe10d0c80983c7d1832152c47a65
ADMIN\Administrator <tien.nguyen20020713@gmail.com>
2024-11-06 20:10:07 +0700

    Complete task CrossEntropy which was checked for all testcases

--- Code/src/ann/loss/CrossEntropy.cpp
+++ Code/src/ann/loss/CrossEntropy.cpp
@@ -13,20 +13,38 @@
 #include "loss/CrossEntropy.h"
 #include "ann/functions.h"
 
-CrossEntropy::CrossEntropy(LossReduction reduction): ILossLayer(reduction){
-    
-}
+CrossEntropy::CrossEntropy(LossReduction reduction) : ILossLayer(reduction){}
 
-CrossEntropy::CrossEntropy(const CrossEntropy& orig):
-ILossLayer(orig){
-}
+CrossEntropy::CrossEntropy(const CrossEntropy& orig) : ILossLayer(orig){}
 
-CrossEntropy::~CrossEntropy() {
-}
+CrossEntropy::~CrossEntropy(){}
 
-double CrossEntropy::forward(xt::xarray<double> X, xt::xarray<double> t){
+double CrossEntropy::forward(xt::xarray<double> X, xt::xarray<double> t)
+{
     //YOUR CODE IS HERE
+    m_aCached_Ypred = X;
+    m_aYtarget = t;
+    const double EPSILON = 1e-7;
+
+    xt::xarray<double> log_Ypred = xt::log(X + EPSILON);
+    xt::xarray<double> loss = -xt::sum(t * log_Ypred);
+
+    if(m_eReduction == REDUCE_MEAN)
+    {
+        return loss()/X.shape()[0];
+    }
+    else return loss();
 }
-xt::xarray<double> CrossEntropy::backward() {
+
+xt::xarray<double> CrossEntropy::backward()
+{
     //YOUR CODE IS HERE
+    const double EPSILON = 1e-7;
+    xt::xarray<double> grad = -m_aYtarget/(m_aCached_Ypred + EPSILON);
+
+    if(m_eReduction == REDUCE_MEAN)
+    {
+        return grad/m_aCached_Ypred.shape()[0];
+    }
+    else return grad;
 }
